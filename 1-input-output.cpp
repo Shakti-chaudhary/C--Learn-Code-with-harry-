2820d367ee9c00f4f880209760a2553e121fd601
Shakti-Chaudhary <shaktichaudhary4321@gmail.com>
2021-06-11 18:20:49 +0530

    Input Output

---
+++ 1-input-output.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ 
+int main(){
+ 
+int a,b;
+
+cin>>a>>b;  /* >> is Called Extration Operator  */
+
+cout<<a+b<<endl; /* << is Called Insertion Operator  */
+ 
+ 
+return 0;
+}
