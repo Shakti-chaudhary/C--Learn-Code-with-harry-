f8237d50a49ba7defb829a07c98a5c5cfbeb0762
Shakti-Chaudhary <shaktichaudhary4321@gmail.com>
2021-06-26 15:27:08 +0530

    Initial commit

---
+++ 4-Control-Structure.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+ 
+int main(){
+ 
+//  ************ if else, else if, Switch case ********************
+
+if (/* condition */)
+{
+    /* code */
+}
+
+else if
+{
+    /* code */
+}
+
+else
+{
+    /* code */
+}
+
+switch (expression)
+{
+case /* constant-expression */:
+    /* code */
+    break;
+
+default:
+    break;
+}
+
+    
+// ************* break and Continue **************************** 
+
+break; /*  Ends the execution of part.  */
+
+continue;  /*  Jumps to next iteration or part.  */
+ 
+ 
+return 0;
+}
